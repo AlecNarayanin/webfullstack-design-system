3c10ef7e95c629c196296db68469be5925c85ed7
dryisf <daryl.issoufi@gmail.com>
2022-05-12 15:39:25 +0200

    feat: git ignore, package json, and add components to public api

--- projects/webfullstack-design-system/src/public-api.ts
+++ projects/webfullstack-design-system/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/webfullstack-design-system.service';
 export * from './lib/webfullstack-design-system.component';
 export * from './lib/webfullstack-design-system.module';
+
+export * from './lib/flex-div/flex-div.component';
+export * from './lib/divider/divider.component';
