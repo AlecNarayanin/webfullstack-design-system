793e40fb1d968ebfe8025243365365ca10f37bde
Alec Narayanin <narayanin.alec@hotmail.com>
2022-05-12 11:41:50 +0200

    Init project lib

---
+++ projects/webfullstack-design-system/src/lib/webfullstack-design-system.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { WebfullstackDesignSystemComponent } from './webfullstack-design-system.component';
+
+
+
+@NgModule({
+  declarations: [
+    WebfullstackDesignSystemComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    WebfullstackDesignSystemComponent
+  ]
+})
+export class WebfullstackDesignSystemModule { }
---
+++ projects/webfullstack-design-system/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of webfullstack-design-system
+ */
+
+export * from './lib/webfullstack-design-system.service';
+export * from './lib/webfullstack-design-system.component';
+export * from './lib/webfullstack-design-system.module';
