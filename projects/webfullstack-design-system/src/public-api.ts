c71d65f5c2290f192171798fb260c7b4549d5125
Alec Narayanin <narayanin.alec@hotmail.com>
2022-05-13 12:13:57 +0200

    feat/button : ajout composant button

--- projects/webfullstack-design-system/src/public-api.ts
+++ projects/webfullstack-design-system/src/public-api.ts
@@ -7,7 +7,13 @@ export * from './lib/webfullstack-design-system.component';
 export * from './lib/webfullstack-design-system.module';
 
 export * from './lib/flex-div/flex-div.component';
+
 export * from './lib/divider/divider.component';
+
 export * from './lib/card/card.component';
+
 export * from './lib/chart/chart.component';
 export * from './lib/chart/chart.interface';
+
+export * from './lib/button/button.component';
+
