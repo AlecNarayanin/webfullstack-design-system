25e2568e9575994551fe0e855aa4991433f445a5
Alec Narayanin <narayanin.alec@hotmail.com>
2022-05-13 15:34:10 +0200

    feat/snackbar : exposition snackbar

--- projects/webfullstack-design-system/src/public-api.ts
+++ projects/webfullstack-design-system/src/public-api.ts
@@ -14,3 +14,5 @@ export * from './lib/chart/chart.interface';
 
 export * from './lib/datatable/datatable.component';
 export * from './lib/datatable/datatable.interface';
+
+export * from './lib/snackbar/snackbar.component';
