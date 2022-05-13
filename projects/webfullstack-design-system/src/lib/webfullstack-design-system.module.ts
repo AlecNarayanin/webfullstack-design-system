93261edd67b2dcd474870b61faf0e324d2735974
Guillaume Zgajnar <guillaume.zgajnar@gonexa.fr>
2022-05-13 10:12:00 +0200

    ajout composant datatable

--- projects/webfullstack-design-system/src/lib/webfullstack-design-system.module.ts
+++ projects/webfullstack-design-system/src/lib/webfullstack-design-system.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { WebfullstackDesignSystemComponent} from "./webfullstack-design-system.component";
 import { CardComponent } from "./card/card.component";
 import { ChartComponent } from './chart/chart.component';
+import { DatatableComponent } from './datatable/datatable.component';
 
-const COMPONENTS = [WebfullstackDesignSystemComponent, CardComponent, ChartComponent];
+const COMPONENTS = [WebfullstackDesignSystemComponent, CardComponent, ChartComponent , DatatableComponent];
 
 @NgModule({
   declarations: [
--- projects/webfullstack-design-system/src/public-api.ts
+++ projects/webfullstack-design-system/src/public-api.ts
@@ -9,3 +9,6 @@ export * from './lib/webfullstack-design-system.module';
 export * from './lib/card/card.component';
 export * from './lib/chart/chart.component';
 export * from './lib/chart/chart.interface';
+
+export * from './lib/datatable/datatable.component';
+export * from './lib/datatable/datatable.interface';
