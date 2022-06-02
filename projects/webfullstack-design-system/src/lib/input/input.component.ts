ea86923997f4a139c611c3592f237ce482125843
Alec Narayanin <narayanin.alec@hotmail.com>
2022-06-02 14:20:55 +0200

    feat/input: retouche input style

--- projects/webfullstack-design-system/src/lib/input/input.component.ts
+++ projects/webfullstack-design-system/src/lib/input/input.component.ts
@@ -1,10 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+import {AbstractControl, ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 @Component({
   selector: 'lib-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements ControlValueAccessor,OnInit {
 
   @Input() type : 'text' | 'password' | 'email' | 'tel' | 'search' | 'number' = 'text';
 
@@ -14,14 +22,45 @@ export class InputComponent implements OnInit {
 
   @Input() value = '';
 
-  constructor() { }
+  @Input() minLength = 0;
 
-  ngOnInit(): void {
+  @Input() maxLength = 10;
+
+  @Input() formControlName = '';
+
+  control!: AbstractControl;
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  constructor(private controlContainer: ControlContainer) { }
+
+  ngOnInit() {
+
+    if(this.controlContainer && this.formControlName){
+      // this.control = this.controlContainer.control.get(this.formControlName);
+    }
 
   }
 
-  // onChange(event) {
-  //   console.log(event);
-  // }
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.valueChange.emit();
+    console.log(changes);
+  }
+
+
+  writeValue(obj: any) {
+    debugger
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any) {
+    debugger
+    this.valueChange.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any) {
+    debugger
+    // this.valueChange.subscribe(fn);
+  }
 
 }
