6e0d227a6a92f00e043174e1cfe5a8050d6eb5e7
dryisf <daryl.issoufi@gmail.com>
2022-06-03 15:34:53 +0200

    feat: add full height prop to flex div

--- projects/webfullstack-design-system/src/lib/flex-div/flex-div.component.ts
+++ projects/webfullstack-design-system/src/lib/flex-div/flex-div.component.ts
@@ -7,12 +7,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FlexDivComponent implements OnInit {
   @Input() fullWidth = false;
+  @Input() fullHeight = false;
   @Input() justifyContent: string = 'start';
   @Input() direction: string = 'row';
   @Input() alignItems: string = 'start';
   @Input() wrap: string = 'nowrap';
 
   constructor() {}
+
+  getClass() {
+    const fullWidthClass = 'flex-div--full-width';
+    const fullHeightClass = 'flex-div--full-height';
+
+    if (this.fullWidth && this.fullHeight) {
+      return `${fullWidthClass} ${fullHeightClass}`;
+    }
+
+    if (this.fullWidth) {
+      return fullWidthClass;
+    }
+
+    if (this.fullHeight) {
+      return fullHeightClass;
+    }
+
+    return '';
+  }
 
   ngOnInit(): void {}
 }
